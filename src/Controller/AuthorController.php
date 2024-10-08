fe81a7264c961e9ebc71a818894452f5bf70b771
Mohamed Slimane <mohamed.slimane@esprit.tn>
2024-10-08 09:57:42 +0100

    crud

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AuthorController extends AbstractController
 {
-    public $authors = array(
-        1 => array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
-        2 => array('id' => 2, 'picture' => '/images/william-shakespeare.jpg', 'username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200),
-        3 => array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),
-    );
+    private array $authors = [
+        1 => ['id' => 1, 'picture' => '/images/Victor-Hugo.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com', 'nb_books' => 100],
+        2 => ['id' => 2, 'picture' => '/images/william-shakespeare.jpg', 'username' => 'William Shakespeare', 'email' => 'william.shakespeare@gmail.com', 'nb_books' => 200],
+        3 => ['id' => 3, 'picture' => '/images/Taha_Hussein.jpg', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300],
+     ];
     #[Route('/author', name: 'app_author')]
     public function index(): Response
     {
@@ -38,4 +43,78 @@ class AuthorController extends AbstractController
 
         return $this->render('author/showDetails.html.twig', ['author' => $author]);
     }
+   #[Route('/author/insertBd', name:'bd_insert')]
+   public function insertBd(ManagerRegistry $manager): Response
+   {
+        $e = $manager->getManager();
+        foreach($this->authors as $author){
+            $newAuthor = new Author();
+            $newAuthor->setUsername($author['username']);
+            $newAuthor->setEmail($author['email']);
+            $e->persist($newAuthor);
+        }
+        $e->flush();
+        return new Response('Authors inserted');
+   }
+   #[Route('/author/showBd', name:'bd_show')]
+    public function showBd(AuthorRepository $authorRepository): Response
+    {
+        $authors = $authorRepository->findAll();
+        return $this->render('author/showBd.html.twig', ['authors' => $authors]);
+    }
+    #[Route('/author/new', name: 'author_new')]
+    public function new(Request $request, ManagerRegistry $manager) : Response 
+    {
+        $author = new Author();
+        $form = $this->createForm(AuthorType::class, $author);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $e = $manager->getManager();
+            $e->persist($author);
+            $e->flush();
+
+            return $this->redirectToRoute('author_success');
+        }
+        return $this->render('author/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    #[Route('/author/success', name: 'author_success')]
+    public function success() 
+    {
+        return new Response('author addes successfully');
+    }
+    #[Route('/author/delete/{id}', name: 'author_delete', methods: ['POST'])]
+    public function delete(Request $request, Author $author, ManagerRegistry $manager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$author->getId(), $request->request->get('_token'))) {
+            $e = $manager->getManager();
+            $e->remove($author);
+            $e->flush();
+        }
+
+        return $this->redirectToRoute('bd_show');
+    }
+    #[Route('/author/edit/{id}', name: 'author_edit')]
+    public function edit(Request $request, Author $author, ManagerRegistry $manager) : Response 
+    {
+        $form = $this->createForm(AuthorType::class, $author);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $e = $manager->getManager();
+            $e->flush();
+
+            return $this->redirectToRoute('bd_show');
+        }
+
+        return $this->render('author/edit.html.twig', [
+            'form' => $form->createView(),
+            'author' => $author,
+        ]);
+    }
+   
 }
